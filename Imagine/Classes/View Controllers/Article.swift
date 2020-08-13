d64fa86c7b2412404fe9387c80eada2b2573232a
Jad <jawlakh_jad@hotmail.com>
2020-08-13 11:28:11 +0300

    Add Article class

---
+++ Imagine/Classes/View Controllers/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  Imagine
+//
+//  Created by Jad Jawlakh on 8/13/20.
+//  Copyright © 2020 Imagine Works. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    
+    var articleId = ""
+    var title = ""
+    var published = ""
+    var sectionName = ""
+    
+}
