50ab80e8928b0f35a09074581e46e983d5903270
Jad <jawlakh_jad@hotmail.com>
2020-08-19 17:53:27 +0300

    Add BookmarkViewModel

---
+++ Imagine/View Controllers/View Models/BookmarkViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarkViewModel.swift
+//  Imagine
+//
+//  Created by Jad Jawlakh on 8/19/20.
+//  Copyright © 2020 Imagine Works. All rights reserved.
+//
+
+import Foundation
+
+class BookmarkViewModel {
+    // Stuff goes here...
+}
