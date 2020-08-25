9224233c894e9d897cf1b436ac27891d8f55f533
Jad <jawlakh_jad@hotmail.com>
2020-08-25 14:41:31 +0300

    Reformat BookmarkButton

--- Imagine/Helpers/BookmarkButton.swift
+++ Imagine/Helpers/BookmarkButton.swift
@@ -9,40 +9,22 @@
 import UIKit
 
 class BookmarkButton: UIButton {
-    static let blue = UIColor(red: 46.0/255.0, green: 123.0/255.0, blue: 255.0/255.0, alpha: 1.0)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initButton()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        initButton()
-    }
-    
-    func initButton() {
-        layer.borderWidth = 2.0
-        layer.borderColor = BookmarkButton.blue.cgColor
-        layer.cornerRadius = frame.size.height/2
-        setTitleColor(BookmarkButton.blue, for: .normal)
-    }
+  static let blue = UIColor(red: 46.0/255.0, green: 123.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    initButton()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    initButton()
+  }
+  
+  func initButton() {
+    layer.borderWidth = 2.0
+    layer.borderColor = BookmarkButton.blue.cgColor
+    layer.cornerRadius = frame.size.height/2
+    setTitleColor(BookmarkButton.blue, for: .normal)
+  }
 }
-
-//        addTarget(self, action: #selector(BookmarkButton.buttonPressed), for: .touchUpInside)
-
-
-//    @objc func buttonPressed() {
-//        guard article.isBookmarked != nil else {
-//            return
-//        }
-//        activateButton(bool: !(article.isBookmarked))
-//    }
-//
-//    func activateButton(bool: Bool) {
-//        article.isBookmarked = bool
-//        let booleanDict: [String: Bool] = ["isArticleBookmarked" : article.isBookmarked]
-//        let title = bool ? "Remove from bookmarks" : "Add to bookmarks"
-//        setTitle(title, for: .normal)
-//        NotificationCenter.default.post(name: Notification.Name("BookmarkButtonPressed"), object: nil, userInfo: booleanDict)
-//    }
