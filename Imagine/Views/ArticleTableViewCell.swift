bc1f8c7d6222e6a428fa3f809596395f687599d9
Jad <jawlakh_jad@hotmail.com>
2020-09-10 09:44:05 +0300

    Add emojis to title, section and date labels in ArticleTableViewCell

--- Imagine/Views/ArticleTableViewCell.swift
+++ Imagine/Views/ArticleTableViewCell.swift
@@ -9,36 +9,36 @@
 import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var sectionLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    var article: Article?
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
   
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
-    }
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var sectionLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  var article: Article?
   
-    func setCell(_ a: Article) {
-        self.article = a
-        // Ensure that we have an article
-        guard self.article != nil else  {
-            return
-        }
-        // Set the title
-        self.titleLabel.text = article?.title
-        // Set the section
-        self.sectionLabel.text = article?.sectionName
-        // Set the date
-        let df = DateFormatter()
-        df.dateFormat = "EEEE, MMM d, yyyy"
-        self.dateLabel.text = df.string(from: article!.published)
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+  
+  func setCell(_ a: Article) {
+    self.article = a
+    // Ensure that we have an article
+    guard self.article != nil else  {
+      return
     }
+    // Set the title
+    self.titleLabel.text = "📰 \(article?.title ?? "")"
+    // Set the section
+    self.sectionLabel.text = "🧭 \(article?.sectionName ?? "")"
+    // Set the date
+    let df = DateFormatter()
+    df.dateFormat = "EEEE, MMM d, yyyy"
+    self.dateLabel.text = "📆 \(df.string(from: article!.published))"
+  }
   
 }
