579fe82101745dfa7a6eb7f30f965057a05c0739
Jad <jawlakh_jad@hotmail.com>
2020-11-04 15:03:33 +0200

    Add SettingsViewController

---
+++ Imagine/View Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Imagine
+//
+//  Created by Jad Jawlakh on 11/4/20.
+//  Copyright © 2020 Imagine Works. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
