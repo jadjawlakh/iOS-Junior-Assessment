5de6fe50778468ef1f33bd5444d202afc4cd7502
Jad <jawlakh_jad@hotmail.com>
2020-11-06 12:59:47 +0200

    Fetch supported Ad Languages from MobAdSDK

--- Imagine/View Controllers/LanguagesViewController.swift
+++ Imagine/View Controllers/LanguagesViewController.swift
@@ -9,82 +9,102 @@
 import UIKit
 
 class LanguagesViewController: UITableViewController {
+  let viewModel = LanguagesViewModel()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    customizeViewController()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    viewModel.loadLanguages { success in
+      self.tableView.reloadData()
+      if !success {
+        // Display an error message
+      }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+  }
+  
+  // MARK: - Table View Functionalities
+  // ==================================
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return viewModel.numberOfSections
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRows(in: section)
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    let info = viewModel.informationForCell(at: indexPath)
+    cell.textLabel?.text = info?.title
+    cell.detailTextLabel?.text = info?.subtitle
+    let shouldSelect = info?.isSelected ?? false
+    cell.isSelectedStyle = shouldSelect
+    if shouldSelect {
+      tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+    } else {
+      tableView.deselectRow(at: indexPath, animated: false)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else {
+      return
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    let success = viewModel.selectLanguage(at: indexPath)
+    cell.isSelectedStyle = success
+  }
+  
+  override func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+    guard viewModel.canDeselectLanguage(at: indexPath) else {
+      return nil
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    return indexPath
+  }
+  
+  override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) else {
+      return
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    let success = viewModel.deselectLanguage(at: indexPath)
+    cell.isSelectedStyle = !success
+  }
+  
+  // MARK: - A C T I O N S
+  // =====================
+  @objc func saveButtonTapped(_ sender: Any) {
+    viewModel.save { (success) in
+      guard success else {
+        // TODO: Display an error message
+        return
+      }
     }
-    */
+  }
+  
+  // MARK: - H E L P E R S
+  // =====================
+  func customizeViewController() {
+    // Rmove lines of empty rows
+    tableView.tableFooterView = UIView()
+    
+    // Add Save bar button
+    let saveBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save, target: self, action: #selector(saveButtonTapped(_:)))
+    navigationItem.rightBarButtonItem = saveBarButton
+  }
+  
+}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+// MARK: - UITableViewCell Extension
+//==================================
+extension UITableViewCell {
+  var isSelectedStyle: Bool {
+    get {
+      self.accessoryType == UITableViewCell.AccessoryType.checkmark
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    set {
+      self.accessoryType = newValue ?
+        UITableViewCell.AccessoryType.checkmark : UITableViewCell.AccessoryType.none
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
--- Imagine/View Controllers/View Models/LanguagesViewModel.swift
+++ Imagine/View Controllers/View Models/LanguagesViewModel.swift
@@ -7,3 +7,91 @@
 //
 
 import Foundation
+import MobAdSDK
+
+typealias LanguagesRowInfo = (title: String, subtitle: String, isSelected: Bool)
+
+class LanguagesViewModel {
+  private(set) var sdkLanguages = [Language]()
+  private(set) var userLanguages = [String]() // Languages codes
+  
+  // MARK: Persist Changes
+  //======================
+  func save(completion: @escaping (_ success: Bool) -> Void) {
+    MobAdSDK.shared.updateUserProfile(countryCode: nil, languageCode: nil, maxAdsPerDay: nil, preferredAdLanguagesCodes: userLanguages) { (success, _, error) in
+      completion(success)
+    }
+  }
+  
+  // MARK: - Loading Data
+  // ====================
+  func loadLanguages(completion: @escaping (_ success: Bool) -> Void) {
+    let group = DispatchGroup()
+    var successFetchSupportedLanguages = false
+    var successFetchUserLanguages = false
+    
+    group.enter()
+    loadSupportedLanguages { (success) in
+      successFetchSupportedLanguages = success
+      group.leave()
+    }
+    group.enter()
+    loadUserLanguages { (success) in
+      successFetchUserLanguages = success
+      group.leave()
+    }
+    group.notify(queue: .main) {
+      completion(successFetchSupportedLanguages && successFetchUserLanguages)
+    }
+  }
+  
+  private func loadSupportedLanguages(completion: @escaping (_ success: Bool) -> Void) {
+    MobAdSDK.shared.getSupportedLanguages { (success, languages, error) in
+      defer {
+        completion(success)
+      }
+      guard let languages = languages else {
+        return
+      }
+      self.sdkLanguages = languages
+    }
+  }
+  
+  private func loadUserLanguages(completion: @escaping (_ success: Bool) -> Void) {
+    userLanguages = MobAdSDK.shared.preferredAdLanguages
+    completion(true)
+  }
+  
+  // MARK: - Table View Functionalities
+  // ==================================
+  func numberOfRows(in section: Int) -> Int {
+    return sdkLanguages.count
+  }
+  var numberOfSections: Int {
+    return sdkLanguages.count > 0 ? 1 : 0
+  }
+  func informationForCell(at indexPath: IndexPath) -> LanguagesRowInfo? {
+    let language = sdkLanguages[indexPath.row]
+    let title = language.name ?? ""
+    let subtitle = language.nativeName ?? ""
+    let isSelected = userLanguages.contains(language.code ?? "")
+    return (title, subtitle, isSelected)
+  }
+  func canDeselectLanguage(at indexPath: IndexPath) -> Bool {
+    return userLanguages.count > 1
+  }
+  func selectLanguage(at indexPath: IndexPath) -> Bool {
+    guard let code = sdkLanguages[indexPath.row].code else {
+      return false
+    }
+    userLanguages.append(code)
+    return true
+  }
+  func deselectLanguage(at indexPath: IndexPath) -> Bool {
+    guard let code = sdkLanguages[indexPath.row].code else {
+      return false
+    }
+    userLanguages.removeAll { $0 == code }
+    return true
+  }
+}
