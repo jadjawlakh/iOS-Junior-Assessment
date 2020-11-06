c4d25b4fa907b07d27f67b923db70511baee6807
Jad <jawlakh_jad@hotmail.com>
2020-11-06 10:34:45 +0200

    Restrict numberOfAdsPerDayTextField input type to numbers only in AdSettingsViewController

--- Imagine/View Controllers/AdSettingsViewController.swift
+++ Imagine/View Controllers/AdSettingsViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class AdSettingsViewController: UIViewController {
+class AdSettingsViewController: UIViewController, UITextFieldDelegate {
   
     override func viewDidLoad() {
+  @IBOutlet weak var numberOfAdsPerDayTextField: UITextField!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
+    numberOfAdsPerDayTextField.delegate = self
     }
+  
+  // MARK: - UITextFieldDelegate
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    let allowedCharacters = "1234567890"
+    let allowedCharacterSet = CharacterSet(charactersIn: allowedCharacters)
+    let typedCharacterSet = CharacterSet(charactersIn: string)
+    return allowedCharacterSet.isSuperset(of: typedCharacterSet)
+    
+  }
 }
